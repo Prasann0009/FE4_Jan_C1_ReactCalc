606378a2ba59c4f3b469565d63a2bbbb68324474
Prasann0009 <prasannlasod0009@gmail.com>
2024-01-13 18:18:23 +0530

    upload

--- src/index.js
+++ src/index.js
@@ -15,10 +15,21 @@ const Calculator = () => {
   };
 
   const validateInput = () => {
-    if (!num1 || !num2 || isNaN(parseFloat(num1)) || isNaN(parseFloat(num2))) {
-      setError('Invalid input. Please enter valid numbers.');
-      return false;
-    }
+     if(!num1)
+     {
+        setError('NUM1 Cannot Be Empty');
+        return false;
+     }
+     else if(!num2)
+     {
+        setError('NUM2 Cannot Be Empty');
+        return false;
+     }
+     else if(isNaN(parseFloat(num1)) || isNaN(parseFloat(num2)))
+     {
+        setError('Invalid input. Please enter valid numbers.');
+        return false;
+     }
     setError('');
     return true;
   };
@@ -69,6 +80,7 @@ const Calculator = () => {
         <h3 style={{ color: 'red' }}>Error!</h3>
        <div>{error}</div>
        </div>}
+        
       {result !== null && <div>
         <h3 style={{ color: 'blue' }}>Success!</h3>
        <div>Result: {result}</div>
