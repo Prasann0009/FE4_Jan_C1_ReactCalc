8df9204fc57a88e28a911552bb1a1dd3256d58b5
Prasann0009 <prasannlasod0009@gmail.com>
2024-01-13 15:39:33 +0530

    upload

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const Hello=()=>{
     let h1style = {textAlign:"center",border:"2px solid black"};
     return (
         <div className="hello">
-            <h1 style={h1style}>Hello World</h1>
+            <h1 style={h1style}>Hello World my dear!</h1>
             <p>{2+3+4+5}</p>
             <p>How are you dear!</p>
         </div>
